2376704469d821926a9bb50e9237a733f4dab4a3
Farha <mahmadfarha43@gmail.com>
2025-02-25 22:36:24 +0530

    Add files via upload

---
+++ Maven_App1/src/main/java/com/insight/App.java
@@ -0,0 +1,13 @@
+package com.insight;
+
+public class App {
+    public void eat(int cukes) {
+    	System.out.println("i am eating");
+    }
+    public void check() {
+    	System.out.println("the condition");
+    }
+    public void result() {
+    	System.out.println("the consequence");
+    }
+}
---
+++ Maven_App1/src/test/java/com/insight/RunCucumber.java
@@ -0,0 +1,16 @@
+package com.insight;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("com.insight")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumber {
+
+}
